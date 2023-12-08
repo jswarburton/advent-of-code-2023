7569aef588ead74f0f81b9bad4e5fbe087e20813
Jonathan Warburton <jonathan.warburton09@gmail.com>
2023-12-08 10:59:01 +0100

    Solve day 1

---
+++ tests/day_01_test.py
@@ -0,0 +1,19 @@
+from main.day_01 import solve_1, solve_2
+
+
+def test_solve_1():
+    input = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+    assert solve_1(input) == 142
+
+
+def test_solve_2():
+    input = [
+        "two1nine",
+        "eightwothree",
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen",
+    ]
+    assert solve_2(input) == 281
