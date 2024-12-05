877232f4803308b88ee6d0c49ff8e0a42245cdcf
Jonathan Warburton <jonathan.warburton09@gmail.com>
2024-12-05 16:01:21 +0100

    Better templating with year

--- setup_day.sh
+++ setup_day.sh
@@ -7,19 +7,20 @@ then
   exit 1
 fi
 
+year="2024"
 day=$1
 
 echo "Creating files for day $day"
 
 padded_day=$(printf "%02d" ${day})
 
-sed -e "s/{{PADDED_DAY}}/${padded_day}/g" template/main_template.txt > main/year2024/day_"${padded_day}".py
+sed -e "s/{{PADDED_DAY}}/${padded_day}/g" -e "s/{{YEAR}}/${year}/g" template/main_template.txt > main/year${year}/day_"${padded_day}".py
 
-sed -e "s/{{PADDED_DAY}}/${padded_day}/g" template/test_template.txt > tests/year2024/day_"${padded_day}"_test.py
+sed -e "s/{{PADDED_DAY}}/${padded_day}/g" -e "s/{{YEAR}}/${year}/g" template/test_template.txt > tests/year${year}/day_"${padded_day}"_test.py
 
-touch tests/year2024/resources/day"${padded_day}".txt
+touch tests/year${year}/resources/day"${padded_day}".txt
 
 session=$(<.session)
-curl --cookie "session=${session}" https://adventofcode.com/2024/day/${day}/input -o main/year2024/resources/day${padded_day}-01.txt
+curl --cookie "session=${session}" https://adventofcode.com/${year}/day/${day}/input -o main/year${year}/resources/day${padded_day}-01.txt
 
 echo "Successfully created files"
